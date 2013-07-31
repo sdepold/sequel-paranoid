610b0ca18c7a5391273425b6b7cfcf2b7a90beab
Sascha Depold <sascha@depold.com>
2013-07-31 07:49:54 +0200

    move models to the spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -11,4 +11,41 @@ RSpec.configure do |config|
     DateTime :deleted_at
     String :name
   end
+
+  DB.create_table(:spec_model_with_default_scopes) do
+    primary_key :id, :auto_increment => true
+    DateTime :deleted_at
+    String :name
+  end
+
+  DB.create_table(:spec_fragments) do
+    primary_key :id, :auto_increment => true
+    Integer :spec_model_id
+    DateTime :deleted_at
+    String :name
+  end
+end
+
+class SpecModel < Sequel::Model
+  plugin :paranoid
+  one_to_many :spec_fragments
+
+  attr_accessor :before_destroy_value, :after_destroy_value
+
+  def before_destroy
+    @before_destroy_value = true
+  end
+
+  def after_destroy
+    @after_destroy_value = true
+  end
+end
+
+class SpecFragment < Sequel::Model
+  plugin :paranoid
+  many_to_one :spec_model
+end
+
+class SpecModelWithDefaultScope < Sequel::Model
+  plugin :paranoid, :set_default_scope => true
 end
